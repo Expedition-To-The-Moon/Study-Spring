e515b0a7a6e710a2500a95c70051e5ea3601ca05
ExpeditionMoon <dnjswon1025@gmail.com>
2024-03-19 16:56:31 +0900

    7. 웹페이지 만들기 - Form
    Form
    - 상품 상세
    - 상품 등록

--- springMVC-1/item-service/src/main/java/hello/itemservice/web/basic/BasicItemController.java
+++ springMVC-1/item-service/src/main/java/hello/itemservice/web/basic/BasicItemController.java
@@ -7,6 +7,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -30,6 +32,26 @@ public class BasicItemController {
         List<Item> items = itemRepository.findAll();
         model.addAttribute("items", items);
         return "basic/items";
+    }
+
+    // 상품 상세
+    @GetMapping("/{itemId}")
+    public String item(@PathVariable("itemId") Long itemId, Model model) {
+        /* PathVariable로 넘어온 상품ID로 상품을 조회하여 model에 담아두고, 뷰 템플릿을 호출함
+           스트링 부트 3.2 부터는 @PathVariable에 "itemId"를 추가해야 정상 작동함 */
+        Item item = itemRepository.findById(itemId);
+        model.addAttribute("item", item);
+        return "basic/item";
+    }
+
+    @GetMapping("/add")
+    public String addForm() {
+        return "basic/addForm";
+    }
+
+    @PostMapping("/add")
+    public String save() {
+        return "";
     }
 
     /**
